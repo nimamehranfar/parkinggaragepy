155e7bca8fa1c886250b4df508489da8383135a3
nimamehranfar <mehranfarnima@rocketmail.com>
2025-11-20 14:33:15 +0100

    us2 red phase

--- test/test_parking_garage.py
+++ test/test_parking_garage.py
@@ -10,8 +10,8 @@ from src.parking_garage import ParkingGarageError
 class TestParkingGarage(TestCase):
 
     @patch.object(GPIO, "input")
-    def test_check_occupancy(self, mock_distance_sensor: Mock):
-        mock_distance_sensor.return_value = True
+    def test_check_occupancy(self, distance_sensor: Mock):
+        distance_sensor.return_value = True
         system = ParkingGarage()
         outcome = system.check_occupancy(system.INFRARED_PIN2)
         self.assertTrue(outcome)
@@ -19,3 +19,9 @@ class TestParkingGarage(TestCase):
     def test_check_occupancy_raises_error(self):
         garage = ParkingGarage()
         self.assertRaises(ParkingGarageError, garage.check_occupancy, garage.LED_PIN)
+
+    def test_get_number_occupied_spots(self,distance_sensor):
+        distance_sensor.side_effect = [True,False,True]
+        garage = ParkingGarage()
+        number_of_occupied_spots = garage.get_number_occupied_spots()
+        self.assertEqual(2, number_of_occupied_spots)
