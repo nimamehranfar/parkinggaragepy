aa8568fbb1b00a66a21092a10f8d40ea3adc247d
nimamehranfar <mehranfarnima@rocketmail.com>
2025-11-20 15:20:57 +0100

    us4 red phase

--- test/test_parking_garage.py
+++ test/test_parking_garage.py
@@ -7,6 +7,7 @@ from mock_folder.SDL_DS3231 import SDL_DS3231
 from src.parking_garage import ParkingGarage
 from src.parking_garage import ParkingGarageError
 
+
 class TestParkingGarage(TestCase):
 
     @patch.object(GPIO, "input")
@@ -21,29 +22,36 @@ class TestParkingGarage(TestCase):
         self.assertRaises(ParkingGarageError, garage.check_occupancy, garage.LED_PIN)
 
     @patch.object(GPIO, "input")
-    def test_get_number_occupied_spots(self,distance_sensor: Mock):
-        distance_sensor.side_effect = [True,False,True]
+    def test_get_number_occupied_spots(self, distance_sensor: Mock):
+        distance_sensor.side_effect = [True, False, True]
         garage = ParkingGarage()
         number_of_occupied_spots = garage.get_number_occupied_spots()
         self.assertEqual(2, number_of_occupied_spots)
 
     @patch.object(SDL_DS3231, "read_datetime")
-    def test_calculate_parking_fee(self,rtc: Mock):
-        rtc.return_value = datetime(2025,11,20,15,24)
+    def test_calculate_parking_fee(self, rtc: Mock):
+        rtc.return_value = datetime(2025, 11, 20, 15, 24)
         garage = ParkingGarage()
-        fee = garage.calculate_parking_fee(datetime(2025,11,20,12,30))
+        fee = garage.calculate_parking_fee(datetime(2025, 11, 20, 12, 30))
         self.assertEqual(7.50, fee)
 
     @patch.object(SDL_DS3231, "read_datetime")
-    def test_calculate_parking_fee_more_minutes(self,rtc: Mock):
-        rtc.return_value = datetime(2025,11,20,15,40)
+    def test_calculate_parking_fee_more_minutes(self, rtc: Mock):
+        rtc.return_value = datetime(2025, 11, 20, 15, 40)
         garage = ParkingGarage()
-        fee = garage.calculate_parking_fee(datetime(2025,11,20,12,30))
+        fee = garage.calculate_parking_fee(datetime(2025, 11, 20, 12, 30))
         self.assertEqual(10, fee)
 
     @patch.object(SDL_DS3231, "read_datetime")
-    def test_calculate_parking_fee_on_weekend(self,rtc: Mock):
-        rtc.return_value = datetime(2025,11,22,15,24)
+    def test_calculate_parking_fee_on_weekend(self, rtc: Mock):
+        rtc.return_value = datetime(2025, 11, 22, 15, 24)
         garage = ParkingGarage()
-        fee = garage.calculate_parking_fee(datetime(2025,11,22,12,30))
+        fee = garage.calculate_parking_fee(datetime(2025, 11, 22, 12, 30))
         self.assertEqual(9.375, fee)
+
+    @patch.object(ParkingGarage, "change_servo_angle")
+    def test_open_garage_door(self, motor: Mock):
+        garage = ParkingGarage()
+        garage.open_garage_door()
+        self.assertTrue(garage.door_open)
+        motor.assert_called_once_with(12)
